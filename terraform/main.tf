81232384fdf0c9832ae15ce2ad7665277220511f
Dmitry Bredikhin <brdm88@gmail.com>
2018-01-13 01:56:20 +0300

    * Fromatted .tf * added .tfvars.example

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,36 +8,36 @@ resource "google_compute_instance" "app" {
   name         = "reddit-app"
   machine_type = "g1-small"
   zone         = "europe-west1-b"
-  
+
   boot_disk {
     initialize_params {
       image = "${var.disk_image}"
     }
   }
-  
+
   network_interface {
-    network = "default"
-    access_config {}
+    network       = "default"
+    access_config = {}
   }
-  
+
   tags = ["reddit-app"]
-  
+
   metadata {
     sshKeys = "brdm88:${file(var.public_key_path)}"
   }
-  
+
   connection {
     type        = "ssh"
     user        = "brdm88"
     agent       = false
     private_key = "${file("C:/Users/brdm/Documents/SSH/GCE.ppk")}"
   }
-  
+
   provisioner "file" {
     source      = "files/puma.service"
     destination = "/tmp/puma.service"
   }
-  
+
   provisioner "remote-exec" {
     script = "files/deploy.sh"
   }
@@ -46,10 +46,12 @@ resource "google_compute_instance" "app" {
 resource "google_compute_firewall" "firewall_puma" {
   name    = "allow-puma-default"
   network = "default"
+
   allow {
     protocol = "tcp"
     ports    = ["9292"]
   }
+
   source_ranges = ["0.0.0.0/0"]
-  target_tags = ["reddit-app"]
+  target_tags   = ["reddit-app"]
 }
