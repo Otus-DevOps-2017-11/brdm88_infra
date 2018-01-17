434c1109c74efc9379c552473d404e80690e975a
Dmitry Bredikhin <brdm88@gmail.com>
2018-01-17 12:56:40 +0300

    Prepared file strucrure for pull request & merge.

--- config-scripts/install_ruby.sh
+++ config-scripts/install_ruby.sh
@@ -4,7 +4,7 @@ set -e
 
 echo "-----------------------------------------------------------------"
 echo `date`: "Installing Ruby..."
-apt update && apt install -y build-essential ruby-full ruby-bundler
+sudo apt update && sudo apt install -y build-essential ruby-full ruby-bundler
 
 echo "================================================================="
 echo "Checking installed Ruby & Bundle versions..."
