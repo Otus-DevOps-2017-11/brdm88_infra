e0540973c081ed26faa1d3fc56b17a0a34378406
Dmitry Bredikhin <brdm88@gmail.com>
2018-01-29 10:21:28 +0300

    Corrected test (bare image) Terraform config

--- terraform/stage_clean/main.tf
+++ terraform/stage_clean/main.tf
@@ -60,7 +60,7 @@ resource "google_compute_firewall" "firewall_puma" {
 
   allow {
     protocol = "tcp"
-    ports    = ["${var.app_port}"]
+    ports    = ["9292"]
   }
 
   source_ranges = ["0.0.0.0/0"]
@@ -120,10 +120,10 @@ resource "google_compute_firewall" "firewall_ssh" {
 
   allow {
     protocol = "tcp"
-    ports    = "${var.ssh_port}"
+    ports    = ["22"]
   }
 
-  source_ranges = "${var.source_ranges}"
+  source_ranges = ["0.0.0.0/0"]
 }
 
 # Definition of project-wide public ssh keys
